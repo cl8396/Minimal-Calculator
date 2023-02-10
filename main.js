9d857f81b0c632849257978372bb5adfcc29b5b2
Curtis <cl8396@gmail.com>
2023-02-10 20:07:31 +0000

    Add operate function

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
-let calculator = {
-    add(a, b){
+function add(a, b){
         return a + b; 
-    },
+    }
 
-    subtract(a, b){
+function subtract(a, b){
         return a - b;
-    },
+    }
 
-    divide(a, b){
+function divide(a, b){
         return a / b;
-    },
+    }
 
-    multiply(a, b){
+function multiply(a, b){
         return a * b;
+    }
+
+function operate(operator, a, b){
+    switch(operator){
+        case '+':
+            add(a, b);
+        case '-':
+            subtract(a, b);
+        case '*':
+            multiply(a, b);
+        case '/':
+            divide(a, b);
     }
 }
 
-console.log(calculator.multiply(8, 10));
