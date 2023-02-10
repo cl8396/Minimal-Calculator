9c87363281d81d053316f3f1f5939da8c3009a2c
Curtis <cl8396@gmail.com>
2023-02-10 16:25:37 +0000

    Add basic operation functionality - console based

---
+++ main.js
@@ -0,0 +1,19 @@
+let calculator = {
+    add(a, b){
+        return a + b; 
+    },
+
+    subtract(a, b){
+        return a - b;
+    },
+
+    divide(a, b){
+        return a / b;
+    },
+
+    multiply(a, b){
+        return a * b;
+    }
+}
+
+console.log(calculator.multiply(8, 10));
